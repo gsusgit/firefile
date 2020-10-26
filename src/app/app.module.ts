d2375cbe726124b1eed9ebcb5f72f5c218170923
Jesus <fernandezjodar.jesus@gmail.com>
2020-10-26 16:14:47 +0100

    Añadido bootstrap, componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CargaComponent } from './pages/carga/carga.component';
+import { FotosComponent } from './pages/fotos/fotos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CargaComponent,
+    FotosComponent
   ],
   imports: [
     BrowserModule,
